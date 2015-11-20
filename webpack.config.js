e03c3462b5e3eef4540a89fcb689caa49b42ab31
rubencosta <rmc@nubolab.org>
2015-11-20 19:11:53 +0000

    initial check in

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.html/, loader: 'file?name=[name].[ext]'},
+      {test: /\.jsx?$/, loader: 'babel', query: {
+        cacheDirectory: true,
+        presets: ['es2015', 'stage-0', 'react']
+      }}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'cycle.js webchat',
+      template: './src/index.tpl.html',
+      inject: true
+    })
+  ]
+}
